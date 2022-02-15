441ecc028cea183f7920d633bec93fcf89aa2695
trossossp3 <21trossos@gmail.com>
2022-02-15 15:22:17 -0500

    final lab3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,8 +1,14 @@
 #include "lab3.h"
 #include <stdio.h>
-//Add TA party item request to the list
-int add_request(struct party_node **head, char *item, double price, char *ta){
-    struct party_node* temp = (struct party_node*)malloc(sizeof(struct party_node));
+// Add TA party item request to the list
+int add_request(struct party_node **head, char *item, double price, char *ta)
+{
+    if (*item == 'I' && *(item + 1) == 'D' && *(item + 2) == 'E' && *(item + 3) == '\0')
+    {
+        return -1;
+    }
+
+    struct party_node *temp = (struct party_node *)malloc(sizeof(struct party_node));
     temp->item = item;
     temp->price = price;
     temp->ta = ta;
@@ -14,44 +20,42 @@ int add_request(struct party_node **head, char *item, double price, char *ta){
     // add the thing to check if word is "IDw\E"
 }
 
-//Remove the last item added
-void remove_request(struct party_node **head){
-    struct party_node* temp = *head;
+// Remove the last item added
+void remove_request(struct party_node **head)
+{
+    struct party_node *temp = *head;
     *head = (*head)->next;
     free(temp);
-
-
 }
 
-//Sort party item requests - in place?
-void make_sorted(struct party_node **head){
+// Sort party item requests - in place?
+void make_sorted(struct party_node **head)
+{
     int swapped;
     struct party_node *curr;
-     
 
     if (*head == NULL)
         return;
-  
+
     do
     {
         swapped = 0;
         curr = *head;
-  
+
         while (curr->next != NULL)
         {
             if (curr->price < curr->next->price)
-            { 
-               
+            {
+
                 swap(curr, curr->next);
                 swapped = 1;
             }
             curr = curr->next;
         }
-       
-    }
-    while (swapped);
+
+    } while (swapped);
 }
-  
+
 /* function to swap data of two nodes a and b*/
 void swap(struct party_node *a, struct party_node *b)
 {
@@ -59,28 +63,55 @@ void swap(struct party_node *a, struct party_node *b)
     a->price = b->price;
     b->price = temp;
 
-    char* temp2 = a->item;
+    char *temp2 = a->item;
     a->item = b->item;
     b->item = temp2;
 
-    char* temp3 = a->ta;
+    char *temp3 = a->ta;
     a->ta = b->ta;
     b->ta = temp3;
-  
 }
 
-//Trim list to fit the budget
-double finalize_list(struct party_node **head, double budget){
-    //Add code here
+// Trim list to fit the budget
+double finalize_list(struct party_node **head, double budget)
+{
+
+    struct party_node *curr = *head;
+    struct party_node *prev = NULL;
+    double cost = 0;
+    while (curr != NULL)
+    {
+        if (cost + curr->price > budget)
+        {
+            if (prev == NULL)
+            { // if we are looking at first index
+                *head = (*head)->next;
+            }
+            else
+            {
+                prev->next = curr->next;
+            }
+        }
+        else
+        {
+            cost += curr->price;
+            prev = curr;
+        }
+        curr = curr->next;
+    }
+
+    return budget-cost;
 }
 
-//Print the current list - hope this is helpful!
-void print_list(struct party_node *head){
+// Print the current list - hope this is helpful!
+void print_list(struct party_node *head)
+{
     int count = 1;
     printf("The current list contains:\n");
-    while(head!=NULL){
+    while (head != NULL)
+    {
         printf("Item %d: %s, %.2lf, requested by %s\n",
-            count, head->item, head->price, head->ta);
+               count, head->item, head->price, head->ta);
         count++;
         head = head->next;
     }
--- lab3/test3.c
+++ lab3/test3.c
@@ -13,53 +13,47 @@ int main(){
     
     check_add = add_request(&head, "Sushi", 2.898, "Ali");
     //Check add_request() return value
-    
+
+
+    if(check_add==-1){
+        printf("Board games was not added.\n");
+    }
+    check_add = add_request(&head, "IDE", 0.00, "TA who will not be named");
+    if(check_add==-1){
+        printf("IDE was not added.\n");
+    }
+    check_add = add_request(&head, "Balloons", 2.49, "Brian");
+    check_add = add_request(&head, "Limousine rental", 500.01, "Tony");
+    check_add = add_request(&head, "Coffee and doughnuts", 10.00, "Nhien");
+    check_add = add_request(&head, "Croissants", 25.50, "Gil");
+    check_add = add_request(&head, "Pineapple pizza", 29.99, "Jeffrey");
+    check_add = add_request(&head, "Charcuterie tray", 79.99, "Samantha");
+    check_add = add_request(&head, "Champagne", 100.00, "Samantha");
+
+    print_list(head); //10 requests added
+
+    //Remove Samantha's request for Champagne
+    printf("LIFO removal:\n");
+    remove_request(&head);
     print_list(head);
+
     
+    //Sort by price
+    printf("Sorting by price:");
     make_sorted(&head);
-    printf("bye");
     print_list(head);
-
-
-    // if(check_add==-1){
-    //     printf("Board games was not added.\n");
-    // }
-    // check_add = add_request(&head, "IDE", 0.00, "TA who will not be named");
-    // if(check_add==-1){
-    //     printf("IDE was not added.\n");
-    // }
-    // check_add = add_request(&head, "Balloons", 2.49, "Brian");
-    // check_add = add_request(&head, "Limousine rental", 500.01, "Tony");
-    // check_add = add_request(&head, "Coffee and doughnuts", 10.00, "Nhien");
-    // check_add = add_request(&head, "Croissants", 25.50, "Gil");
-    // check_add = add_request(&head, "Pineapple pizza", 29.99, "Jeffrey");
-    // check_add = add_request(&head, "Charcuterie tray", 79.99, "Samantha");
-    // check_add = add_request(&head, "Champagne", 100.00, "Samantha");
-
-    // print_list(head); //10 requests added
-
-    // //Remove Samantha's request for Champagne
-    // printf("LIFO removal:\n");
-    // remove_request(&head);
-    // print_list(head);
-
     
-    // //Sort by price
-    // printf("Sorting by price:");
-    // make_sorted(&head);
-    // print_list(head);
-    
-    // //Finalize the shopping list
-    // printf("Finalizing the shopping list:\n");
-    // double remainder=0.00;
-    // remainder = finalize_list(&head, 500.00);
-    // print_list(head);
-    // printf("The remainder after shopping is: %.2lf\n", remainder);
+    //Finalize the shopping list
+    printf("Finalizing the shopping list:\n");
+    double remainder=0.00;
+    remainder = finalize_list(&head, 500.00);
+    print_list(head);
+    printf("The remainder after shopping is: %.2lf\n", remainder);
 
-    // //Free the list
-    // while(head!=NULL){
-    //     remove_request(&head);
-    // }
-    // printf("Ready to party!\n");
+    //Free the list
+    while(head!=NULL){
+        remove_request(&head);
+    }
+    printf("Ready to party!\n");
     return 0;
 }
